91756dd925e66cf40db2666e76286d1f41934589
PedroJaber <80411582+Pedro-Jaber@users.noreply.github.com>
2023-10-13 04:18:47 +0000

    add `/clara` route

---
+++ pages/clara/index.js
@@ -0,0 +1,20 @@
+function Home() {
+  // return <h1>[insira uma frase muito impactante aqui] 😎</h1>
+  return (
+    <div>
+      <center>
+        <h1> Te amo amooor 😍💗❤️</h1>
+        <img
+          src="https://media.tenor.com/cWFdc87CgioAAAAC/aaaaaaaaaaaa.gif"
+          height={300}
+        ></img>
+        <img
+          src="https://media.tenor.com/FMaN1r57kukAAAAM/love.gif"
+          height={300}
+        ></img>
+      </center>
+    </div>
+  );
+}
+
+export default Home;
