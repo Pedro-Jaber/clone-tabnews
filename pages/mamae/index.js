0e8e318aa1e19a3a9d707c8db8f6744dfc79a925
PedroJaber <80411582+Pedro-Jaber@users.noreply.github.com>
2023-10-13 04:18:56 +0000

    add `/mamae` route

---
+++ pages/mamae/index.js
@@ -0,0 +1,16 @@
+function Home() {
+  // return <h1>[insira uma frase muito impactante aqui] 😎</h1>
+  return (
+    <div>
+      <center>
+        <h1> Mamãe linda do meu coração eu te amo amooor 😍❤️</h1>
+        <img
+          src="https://media.tenor.com/FMaN1r57kukAAAAM/love.gif"
+          height={300}
+        ></img>
+      </center>
+    </div>
+  );
+}
+
+export default Home;
